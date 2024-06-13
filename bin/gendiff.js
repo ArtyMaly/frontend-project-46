39f9ebecf2a0052b2af2e1f38af8a00bbce0b854
ArtyMaly <artemmaly@mail.ru>
2024-06-13 12:17:50 +0300

    step5

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -2,13 +2,14 @@
 
 import { Command } from 'commander';
 import gendiff from '../index.js';
+
 const program = new Command();
 
 program
   .name('gendiff')
   .description('Compares two configuration files and shows a difference.')
   .version('1.0.0')
-  .option('-f, --format [type]','output format')
+  .option('-f, --format [type]', 'output format')
   .arguments('<filepath1> <filepath2>')
   .action((filepath1, filepath2) => {
     console.log(gendiff(filepath1, filepath2));
