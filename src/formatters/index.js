e9b287a0dae97e22d8b71374ebeff623e4dd2a17
ArtyMaly <artemmaly@mail.ru>
2024-12-09 14:59:46 +0300

    step8

---
+++ src/formatters/index.js
@@ -0,0 +1,11 @@
+import stringifyStylish from './stylish.js';
+import stringifyPlain from './plain.js';
+
+export default (formatName) => {
+  switch (formatName) {
+    case 'plain':
+      return stringifyPlain;
+    default:
+      return stringifyStylish;
+  }
+};
